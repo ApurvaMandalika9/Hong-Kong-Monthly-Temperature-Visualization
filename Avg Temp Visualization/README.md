d11f74a3096744791b457e971e8c70afa16b1a84
ApurvaMandalika9 <apurva.mandalika96@gmail.com>
2025-03-17 12:40:49 -0500

    Adding separate Min/Max and Avg  Visualization codes

--- Avg Temp Visualization/README.md
+++ Avg Temp Visualization/README.md
@@ -9,7 +9,7 @@ The dataset used is uploaded as temperature_daily.csv.
 
 Key features -
 1. In the matrix, x direction indicates the year and y direction indicates the month. Each cell indicates the corresponding month of a specific year.
-2. Added color to each cell visualizing the maximum and minimum temperature by month. A button can be clicked to switch between the visualizations for Min and Max.
+2. Added color to each cell such that it represents the monthly average temperature. This value was computed by first calculating the daily average temperature (the sum of the max and min temperatures divided by 2) and then determining the arithmetic mean of these daily averages for each month.
 3. When the mouse pointer is hovered on any cell, a tooltip would appear showing the date(Year-Month), min and max temperature values.
 4. A legend showing the mapping between colors and values was also added.
 
@@ -20,6 +20,6 @@ In this level, even within each month, the daily trends of max and min temperatu
 
 Key features -
 1. Same as level 1, x direction indicates the year and y direction indicates the month. Each cell indicates the corresponding month of a specific year.
-2. Still same as level 1, added color to each cell such that it represents the monthly Max/Min temperatures.
+2. Still same as level 1, added color to each cell such that it represents the monthly average temperature.
 3. In the mini line charts within each cell, x direction represents the days in a month, and y direction represents the temperature
 4. A legend to show the mapping between colors and values was also added.
--- Avg Temp Visualization/script.js
+++ Avg Temp Visualization/script.js
@@ -1,17 +1,15 @@
-// Global variables for aggregated datasets, current level, and temperature mode.
+// Creating the global variables to store the aggregated datasets and current level.
 let aggregatedDataLevel1, aggregatedDataLevel2;
 let currentLevel = "level1";
-let currentTempMode = "max";
 
 // Creating a container with buttons and visualization area.
 d3.select("body").append("div").html(
   `<button id='level1' class='selected'>Level 1</button> 
    <button id='level2'>Level 2</button>
-   <button id='tempToggle'>Show Min</button>
    <div id='visualization'></div>`
 );
 
-// Adding button events for switching levels.
+// Adding button events, rendering the matrix again when the level changes.
 d3.select("#level1").on("click", () => {
     d3.selectAll("button").classed("selected", false);
     d3.select("#level1").classed("selected", true);
@@ -27,16 +25,9 @@ d3.select("#level2").on("click", () => {
 });
 
 
-// Adding button event for toggling temperature mode.
-d3.select("#tempToggle").on("click", () => {
-    currentTempMode = currentTempMode === "max" ? "min" : "max";
-    d3.select("#tempToggle").text("Show " + (currentTempMode === "max" ? "Min" : "Max"));
-    renderMatrix();
-});
-
-// Loading the CSV data.
+// Loading the CSV data
 d3.csv("temperature_daily.csv").then(data => {
-  // Parsing each CSV row.
+  // Parsing each CSV row
   data.forEach(d => {
     d.date = new Date(d.date);
     d.year = d.date.getFullYear();
@@ -46,8 +37,7 @@ d3.csv("temperature_daily.csv").then(data => {
     d.avg_temperature = (d.max_temperature + d.min_temperature) / 2;
   });
 
-  // Creating two aggregated datasets: 
-  // Level 1: filtering years >= 1997; Level 2: filtering years >= 2008.
+  // Creating two aggregated datasets: 1. For Level 1: filtering years >= 1997; 2. For Level 2: filtering years >= 2008.
   const aggregateByYearMonth = (filterYear) => {
     const filtered = data.filter(d => d.year >= filterYear);
     return d3.rollups(filtered, v => ({
@@ -65,38 +55,36 @@ d3.csv("temperature_daily.csv").then(data => {
   aggregatedDataLevel1 = aggregateByYearMonth(1997);
   aggregatedDataLevel2 = aggregateByYearMonth(2008);
 
-  // Initially rendering Level 1 by default.
+  // Initially rendering Level 1 by default
   renderMatrix();
 });
 
-// Function to render the entire visualization.
+// This function is for rendering the entire visualization.
 function renderMatrix() {
   // Removing any existing SVG and tooltip.
   d3.select("#visualization").selectAll("svg").remove();
   d3.select("#visualization").selectAll(".tooltip").remove();
 
-  // Determining the aggregated data based on the current level.
+  // Determining the aggregated data to be used, based on the current level.
   const aggregatedData = currentLevel === "level1" ? aggregatedDataLevel1 : aggregatedDataLevel2;
 
-  // Setting up margins and dimensions.
-  const margin = { top: 100, right: 100, bottom: 100, left: 100 },
-        width = 800 - margin.left - margin.right,
-        height = 600 - margin.top - margin.bottom;
+  // Setting up the margins and dimensions.
+  const margin = { top: 100, right: 100, bottom: 100, left: 100 }, width = 800 - margin.left - margin.right, height = 600 - margin.top - margin.bottom;
 
   // Creating the main SVG container.
   const svg = d3.select("#visualization")
     .append("svg")
-    .attr("width", width + margin.left + margin.right + 100)
+    .attr("width", width + margin.left + margin.right+100)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // Determining the number of years in the aggregated data.
+  // Determining the number of years present in the aggregated data for the current level
   const years = Array.from(new Set(aggregatedData.map(d => d.year))).sort();
-  // Using months 1 through 12.
+  // For months, use 1-12.
   const months = d3.range(1, 13);
 
-  // Defining the scales for the matrix.
+  // Defining the scales for the matrix
   const xScale = d3.scaleBand()
     .domain(years)
     .range([0, width])
@@ -107,7 +95,10 @@ function renderMatrix() {
     .range([0, height])
     .padding(0.05);
 
-  // Set the global min and max temperatures (in Celsius) for the color scale.
+
+  // Below code is for adding the base matrix and then adding the colored cells based on avg temp
+
+  // Setting the global min to 0 Celsius and the global maximum to 40 Celsius
   const globalMinTemp = 0;
   const globalMaxTemp = 40;
   const colorScale = d3.scaleSequential(d3.interpolateYlOrRd)
@@ -125,12 +116,11 @@ function renderMatrix() {
     .attr("class", "cell")
     .attr("transform", d => `translate(${xScale(d.year)},${yScale(d.month)})`);
 
-  // Adding a colored rectangle for each cell.
-  // Now, the fill is based on the currentTempMode (max or min).
+  // Adding a colored rectangle for each cell based on the avg temperature of the month
   cells.append("rect")
     .attr("width", xScale.bandwidth())
     .attr("height", yScale.bandwidth())
-    .attr("fill", d => d[currentTempMode] !== undefined ? colorScale(d[currentTempMode]) : "#ccc")
+    .attr("fill", d => d.avg !== undefined ? colorScale(d.avg) : "#ccc")
     .on("mouseover", function(event, d) {
       tooltip.style("visibility", "visible")
         .html(`Date: ${d.year}-${d.month}<br>Max Temp: ${d.max}°C<br>Min Temp: ${d.min}°C`);
@@ -141,7 +131,7 @@ function renderMatrix() {
     )
     .on("mouseout", () => tooltip.style("visibility", "hidden"));
 
-  // Creating a tooltip div.
+  // Creating a tooltip div for disaplying the values upon hover
   const tooltip = d3.select("#visualization").append("div")
     .attr("class", "tooltip")
     .style("position", "absolute")
@@ -158,7 +148,8 @@ function renderMatrix() {
   svg.append("g")
     .call(d3.axisLeft(yScale).tickFormat(d => d3.timeFormat("%B")(new Date(0, d - 1))));
 
-  // Creating a legend for the color scale.
+  // Below code is for creating a legend for the color scale - Dimensions, type, scale and values
+
   const legendHeight = 150,
         legendWidth = 20;
   const legend = svg.append("g")
@@ -179,35 +170,35 @@ function renderMatrix() {
     .attr("y", d => (d / 10) * legendHeight)
     .attr("width", legendWidth)
     .attr("height", legendHeight / 10)
-    .attr("fill", d => colorScale(legendScale.invert((d / 10) * legendHeight)));
+    .attr("fill", d => colorScale(legendScale.invert(d / 10 * legendHeight)));
 
   legend.append("g")
     .attr("transform", `translate(${legendWidth}, 0)`)
     .call(legendAxis);
 
-  // If currently in Level 2, add the mini line charts.
+  // If currently in Level 2, adding the mini line charts.
   if (currentLevel === "level2") {
     cells.each(function(d) {
-      // Only add trends if there are enough daily data points.
+      // Only adding trends if there are enough daily data points.
       if (d.days && d.days.length > 1) {
         // Creating a scale for the x-axis of the mini chart within the cell.
         const dayScale = d3.scaleLinear()
           .domain([0, d.days.length - 1])
           .range([0, xScale.bandwidth()]);
 
-        // Line for the min temperatures.
+        // Line for the min temperatures (light blue).
         const lineMin = d3.line()
           .x((day, i) => dayScale(i))
           .y(day => cellTempScale(day.min_temperature))
           .curve(d3.curveMonotoneX);
 
-        // Line for the max temperatures.
+        // Line for the max temperatures (green).
         const lineMax = d3.line()
           .x((day, i) => dayScale(i))
           .y(day => cellTempScale(day.max_temperature))
           .curve(d3.curveMonotoneX);
 
-        // Append the min temperature trend line.
+        // Appending the min temperature trend line.
         d3.select(this).append("path")
           .datum(d.days)
           .attr("class", "trend")
@@ -216,7 +207,7 @@ function renderMatrix() {
           .attr("stroke-width", 1.5)
           .attr("d", lineMin);
 
-        // Append the max temperature trend line.
+        // Appending the max temperature trend line.
         d3.select(this).append("path")
           .datum(d.days)
           .attr("class", "trend")
@@ -225,12 +216,14 @@ function renderMatrix() {
           .attr("stroke-width", 1.5)
           .attr("d", lineMax);
 
-        // Adding a legend for the trend lines.
         const trendLegend = svg.append("g")
           .attr("class", "trend-legend")
+          // Position this legend relative to the main legend. Adjust as needed.
           .attr("transform", `translate(${width + 30}, ${height - legendHeight - 50 - 100})`);
       
-        // Blue swatch for Min Temperature.
+        const swatchSize = 15;
+      
+        // Blue swatch for Min Temperature
         trendLegend.append("line")
           .attr("x1", 0)
           .attr("x2", 20)
@@ -246,14 +239,15 @@ function renderMatrix() {
           .attr("font-size", "12px")
           .attr("alignment-baseline", "middle");
       
-        // Green swatch for Max Temperature.
+        // Green swatch for Max Temperature
         trendLegend.append("line")
-          .attr("x1", 0)
-          .attr("x2", 20)
-          .attr("y1", 60)
-          .attr("y2", 60)
-          .attr("stroke", "green")
-          .attr("stroke-width", 2);
+           .attr("x1", 0)
+           .attr("x2", 20)
+           .attr("y1", 60)
+           .attr("y2", 60)
+           .attr("stroke", "green")
+           .attr("stroke-width", 2);
+          
       
         trendLegend.append("text")
           .attr("x", 25)
